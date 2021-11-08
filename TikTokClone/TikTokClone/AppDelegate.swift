5830b6addca2268be46d9b4a5cd0169505c4c1ae
Hiram Castro <hiramcastrodeveloper@gmail.com>
2021-11-07 16:16:18 -0800

    configuring app orientation and setting up main root VC

--- TikTokClone/TikTokClone/AppDelegate.swift
+++ TikTokClone/TikTokClone/AppDelegate.swift
@@ -9,11 +9,21 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        var rootVC:UIViewController!
+        rootVC = UINavigationController(rootViewController: ViewController())
+        
+        window.rootViewController = rootVC
+        
+        self.window = window
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
