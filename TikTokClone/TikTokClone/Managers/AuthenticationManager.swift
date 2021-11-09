de55629433b1967d7cdb50b7ff515c1c4b03a527
Hiram Castro <hiramcastrodeveloper@gmail.com>
2021-11-08 22:15:43 -0800

    adding initial setup for managers - needs fix for high coupling using  singletons

--- TikTokClone/TikTokClone/Managers/AuthenticationManager.swift
+++ TikTokClone/TikTokClone/Managers/AuthenticationManager.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+final class AuthManager {
+    
+    enum SignInMethod {
+        case email
+        case facebook
+        case google
+    }
+    
+    static let shared = AuthManager()
+    
+    private init() { }
+    
+    public func signIn(with method: SignInMethod) {
+        
+    }
+    
+    public func signOut() {
+        
+    }
+    
+}
